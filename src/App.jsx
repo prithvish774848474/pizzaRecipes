944478cd071f288b7c25a1bb8b782b696605e16f
prithvish774848474 <prithvish.freelance@gmail.com>
2024-06-24 23:46:57 +0530

    Initializing Git with VS Code Project

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useRef } from "react";
+import "./App.css";
+import { recipes } from "./data.js";
+
+function SearchList({ updateRecipeList }) {
+  const handleOptionChange = (e) => updateRecipeList((previousRecipeList) =>
+    previousRecipeList.filter((recipe) => recipe.title === e.target.value)
+  );
+  return (
+    <div className="header">
+      <p className="introduction">Welcome to the <span>World of Pizzas</span></p>
+      <select className="searchBar" onChange={handleOptionChange}>
+        <option value="">
+          {" "}
+          --Select--{" "}
+        </option>
+        {recipes.map((recipe) => {
+          return (
+            <option
+              key={recipe.id}
+              value={recipe.title}
+            >
+              {recipe.title}
+            </option>
+          );
+        })}
+      </select>
+      <button
+        type="button"
+        className="reset"
+        onClick={() => updateRecipeList(recipes)}
+      >
+        Reset!
+      </button>
+    </div>
+  );
+}
+
+function RecipeContainer({ recipeList }) {
+  return (
+    <div className="recipe-container">
+      {recipeList.map((recipe) => {
+        return <RecipeCard key={recipe.id} recipe={recipe} />;
+      })}
+    </div>
+  );
+}
+
+function RecipeCard({ recipe }) {
+  return (
+    <div className="recipe-card">
+      <img
+        src={recipe.image_url}
+        alt={recipe.title}
+        className="recipe-card-image"
+      />
+      <p className="recipe-card-title">{recipe.title}</p>
+      <button
+        type="button"
+        className="recipe-card-redirect"
+        onClick={() => window.open(recipe.recipe_url)}
+      >
+        View Full Recipe <br />
+        <span>({recipe.publisher})</span>
+      </button>
+    </div>
+  );
+}
+
+export default function App() {
+  const [recipeList, setRecipeList] = useState(recipes);
+  return (
+    <>
+      <SearchList updateRecipeList={setRecipeList} />
+      <RecipeContainer recipeList={recipeList} />
+    </>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,384 @@
+export const recipes = [
+  {
+    publisher: "Closet Cooking",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/BBQChickenPizzawithCauliflowerCrust5004699695624ce.jpg",
+    title: "Cauliflower Pizza Crust (with BBQ Chicken Pizza)",
+    recipe_url:
+      "https://www.closetcooking.com/cauliflower-pizza-crust-with-bbq/",
+    id: "664c8f193e7aa067e94e8706",
+  },
+  {
+    publisher: "Simply Recipes",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/howtogrillpizzad300x20086a60e1b.jpg",
+    title: "How to Grill Pizza",
+    recipe_url: "https://www.simplyrecipes.com/recipes/how_to_grill_pizza/",
+    id: "664c8f193e7aa067e94e8534",
+  },
+  {
+    publisher: "Simply Recipes",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/pizza292x2007a259a79.jpg",
+    title: "Homemade Pizza",
+    recipe_url: "https://www.simplyrecipes.com/recipes/homemade_pizza/",
+    id: "664c8f193e7aa067e94e8531",
+  },
+  {
+    publisher: "Closet Cooking",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/Pizza2BDip2B12B500c4c0a26c.jpg",
+    title: "Pizza Dip",
+    recipe_url: "https://www.closetcooking.com/pizza-dip/",
+    id: "664c8f193e7aa067e94e84c1",
+  },
+  {
+    publisher: "All Recipes",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/IMG_4351180x1804f4a.jpg",
+    title: "Greek Pizza",
+    recipe_url: "https://www.allrecipes.com/recipe/232032/greek-pizza/",
+    id: "664c8f193e7aa067e94e8438",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/391236ba85.jpg",
+    title: "Veggie Pizza",
+    recipe_url: "https://www.allrecipes.com/recipe/15022/veggie-pizza/",
+    id: "664c8f193e7aa067e94e845a",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/7988559586.jpg",
+    title: "Valentine Pizza",
+    recipe_url:
+      "https://www.allrecipes.com/recipes/199/holidays-and-events/valentines-day/",
+    id: "664c8f193e7aa067e94e8454",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/5100898cc5.jpg",
+    title: "Pizza Casserole",
+    recipe_url: "https://www.allrecipes.com/recipe/18781/pizza-casserole/",
+    id: "664c8f193e7aa067e94e836b",
+  },
+  {
+    publisher: "BBC Food",
+    image_url: "http://forkify-api.herokuapp.com/images/1649634_MEDIUMd3fc.jpg",
+    title: "Pitta pizzas",
+    recipe_url: "https://www.bbc.co.uk/food/recipes/pitta_pizzas_20075",
+    id: "664c8f193e7aa067e94e838d",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/567c8fe.jpg",
+    title: "Pizza Pinwheels",
+    recipe_url: "https://www.allrecipes.com/recipe/23702/pizza-pinwheels/",
+    id: "664c8f193e7aa067e94e836e",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/104254d419.jpg",
+    title: "Pesto Pizza",
+    recipe_url: "https://www.allrecipes.com/recipe/21528/pesto-pizza/",
+    id: "664c8f193e7aa067e94e8355",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/636003da23.jpg",
+    title: "Hummus Pizza",
+    recipe_url: "https://www.allrecipes.com/recipe/74437/hummus-pizza/",
+    id: "664c8f193e7aa067e94e82fa",
+  },
+  {
+    publisher: "What's Gaby Cooking",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/PizzaMonkeyBread67f8.jpg",
+    title: "Pizza Monkey Bread",
+    recipe_url: "https://whatsgabycooking.com/pizza-monkey-bread/",
+    id: "664c8f193e7aa067e94e8ab9",
+  },
+  {
+    publisher: "BBC Food",
+    image_url: "http://forkify-api.herokuapp.com/images/679637_MEDIUM765c.jpg",
+    title: "Puff Pizza Tart",
+    recipe_url: "https://www.bbc.co.uk/food/recipes/puff_pastry_tarts_28261",
+    id: "664c8f193e7aa067e94e8abd",
+  },
+  {
+    publisher: "Epicurious",
+    image_url: "http://forkify-api.herokuapp.com/images/51150600f4cb.jpg",
+    title: "Veggi-Prosciutto Pizza",
+    recipe_url:
+      "https://www.epicurious.com/recipes/food/views/veggi-prosciutto-pizza-51150600",
+    id: "664c8f193e7aa067e94e89af",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    image_url: "http://forkify-api.herokuapp.com/images/pizza3464.jpg",
+    title: "Pizza Potato Skins",
+    recipe_url:
+      "https://www.thepioneerwoman.com/food-cooking/recipes/a11383/pizza-potato-skins/",
+    id: "664c8f193e7aa067e94e88b9",
+  },
+  {
+    publisher: "Pillsbury",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/minifruitpizzas52c00.jpg",
+    title: "Mini Fruit Pizzas",
+    recipe_url:
+      "https://www.pillsbury.com/recipes/mini-fruit-pizzas/fe1c8bba-09ad-46fe-85f7-782c65b703b6",
+    id: "664c8f193e7aa067e94e880a",
+  },
+  {
+    publisher: "Simply Recipes",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/nokneadpizzadoughlahey6461467.jpg",
+    title: "No-Knead Pizza Dough",
+    recipe_url:
+      "https://www.simplyrecipes.com/recipes/easy_no_knead_pizza_dough/",
+    id: "664c8f193e7aa067e94e8783",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/4797377235_c07589b7d4_be953.jpg",
+    title: "Mexican Flatbread Pizza",
+    recipe_url:
+      "https://www.thepioneerwoman.com/food-cooking/recipes/a9405/16-minute-meal-4-mexican-flatbread-pizza/",
+    id: "664c8f193e7aa067e94e86af",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    image_url: "http://forkify-api.herokuapp.com/images/grilledveggie79bd.jpg",
+    title: "Grilled Veggie Pizza",
+    recipe_url:
+      "https://www.thepioneerwoman.com/food-cooking/recipes/a11363/grilled-vegetable-pizza/",
+    id: "664c8f193e7aa067e94e867b",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    image_url: "http://forkify-api.herokuapp.com/images/burger53be.jpg",
+    title: "Supreme Pizza Burgers",
+    recipe_url:
+      "https://www.thepioneerwoman.com/food-cooking/recipes/a11757/supreme-pizza-burgers/",
+    id: "664c8f193e7aa067e94e863b",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    image_url: "http://forkify-api.herokuapp.com/images/pizzaburgera5bd.jpg",
+    title: "Pepperoni Pizza Burgers",
+    recipe_url:
+      "https://www.thepioneerwoman.com/food-cooking/recipes/a11758/pepperoni-pizza-burgers/",
+    id: "664c8f193e7aa067e94e863d",
+  },
+  {
+    publisher: "Closet Cooking",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/Taco2BQuesadilla2BPizza2B5002B4417a4755e35.jpg",
+    title: "Taco Quesadilla Pizzas",
+    recipe_url: "https://www.closetcooking.com/taco-quesadilla-pizza/",
+    id: "664c8f193e7aa067e94e84de",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/580542e3ec.jpg",
+    title: "Hot Pizza Dip",
+    recipe_url: "https://www.allrecipes.com/recipe/14803/hot-pizza-dip/",
+    id: "664c8f193e7aa067e94e82f4",
+  },
+  {
+    publisher: "All Recipes",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/30624_RecipeImage_620x413_pepperoni_pizza_dip_4774d.jpg",
+    title: "Pepperoni Pizza Dip Recipe",
+    recipe_url:
+      "https://www.allrecipes.com/recipe/8542273/pepperoni-pizza-dip/",
+    id: "664c8f193e7aa067e94e8a31",
+  },
+  {
+    publisher: "What's Gaby Cooking",
+    image_url: "http://forkify-api.herokuapp.com/images/IMG_15866d21.jpg",
+    title: "Grilled BBQ Chicken Pizza",
+    recipe_url: "https://whatsgabycooking.com/grilled-bbq-chicken-pizza/",
+    id: "664c8f193e7aa067e94e8a12",
+  },
+  {
+    publisher: "All for Pizza",
+    image_url: "http://forkify-api.herokuapp.com/images/2150654_MEDIUM6068.jpg",
+    title: "Pizza Bianca with Artichoke Hearts",
+    recipe_url: "https://allforpizza.com/pizza-bianca-with-artichoke-recipe/",
+    id: "664c8f193e7aa067e94e897b",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/4364270576_302751a2a4f3c1.jpg",
+    title: "My Favorite Pizza",
+    recipe_url:
+      "https://www.thepioneerwoman.com/food-cooking/recipes/a11366/my-favorite-pizza/",
+    id: "664c8f193e7aa067e94e86ba",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/steakhousepizza0b87.jpg",
+    title: "One Basic Pizza Crust",
+    recipe_url:
+      "https://www.thepioneerwoman.com/food-cooking/recipes/a36080815/basic-pizza-dough-recipe/",
+    id: "664c8f193e7aa067e94e8673",
+  },
+  {
+    publisher: "The Pioneer Woman",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/5278973957_3f9f9a21c2_o7a1b.jpg",
+    title: "Fig-Prosciutto Pizza with Arugula",
+    recipe_url:
+      "https://www.thepioneerwoman.com/food-cooking/recipes/a11362/fig-prosciutto-pizza-with-arugula/",
+    id: "664c8f193e7aa067e94e866f",
+  },
+  {
+    publisher: "Cleaver Housewife",
+    image_url: "http://forkify-api.herokuapp.com/images/fruitpizza9a19.jpg",
+    title: "Deep Dish Fruit Pizza",
+    recipe_url: "https://cleverhousewife.com/2014/05/deep-dish-fruit-pizza/",
+    id: "664c8f193e7aa067e94e8658",
+  },
+  {
+    publisher: "Slurrp",
+    image_url: "http://forkify-api.herokuapp.com/images/pizza_30061a5d763.jpg",
+    title: "Salami and Brussels Sprouts Pizza",
+    recipe_url:
+      "https://www.slurrp.com/recipes/salami-brussels-sprouts-pizza-1623843982",
+    id: "664c8f193e7aa067e94e8605",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/pizza_300d938bd58.jpg",
+    title: "English-Muffin Egg Pizzas",
+    recipe_url:
+      "https://www.allrecipes.com/recipe/86649/fast-english-muffin-pizzas/",
+    id: "664c8f193e7aa067e94e85be",
+  },
+  {
+    publisher: "Budget Bytes",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/Pizza2BQuesadillas2B2528aka2BPizzadillas25292B5002B834037bf306b.jpg",
+    title: "Pizza Quesadillas (aka Pizzadillas)",
+    recipe_url: "https://www.budgetbytes.com/pizzadillas/",
+    id: "664c8f193e7aa067e94e84c2",
+  },
+  {
+    publisher: "Tasty",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/PepperoniPizzaMonkeyBread8cd5.jpg",
+    title: "Pepperoni Pizza Monkey Bread",
+    recipe_url: "https://tasty.co/recipe/pepperoni-pizza-monkey-bread",
+    id: "664c8f193e7aa067e94e8433",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/191121d99d.jpg",
+    title: "Fast English Muffin Pizzas",
+    recipe_url:
+      "https://www.allrecipes.com/recipe/86649/fast-english-muffin-pizzas/",
+    id: "664c8f193e7aa067e94e82b4",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/100111309d9.jpg",
+    title: "Double Crust Stuffed Pizza",
+    recipe_url:
+      "https://www.allrecipes.com/recipe/184068/double-crust-stuffed-pizza/",
+    id: "664c8f193e7aa067e94e8297",
+  },
+  {
+    publisher: "My Baking Addiction",
+    image_url: "http://forkify-api.herokuapp.com/images/FlatBread21of1a180.jpg",
+    title: "Spicy Chicken and Pepper Jack Pizza",
+    recipe_url:
+      "https://www.mybakingaddiction.com/spicy-chicken-and-pepper-jack-pizza-recipe/",
+    id: "5ed6604591c37cdc054bc886",
+  },
+  {
+    publisher: "All Recipes",
+    image_url: "http://forkify-api.herokuapp.com/images/237891b5e4.jpg",
+    title: "Jay's Signature Pizza Crust",
+    recipe_url:
+      "https://www.allrecipes.com/recipe/7245/jays-signature-pizza-crust/",
+    id: "5ed6604591c37cdc054bc990",
+  },
+  {
+    publisher: "Two Peas and Their Pod",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/peachbasilpizza6c7de.jpg",
+    title: "Peach, Basil, Mozzarella, & Balsamic Pizza",
+    recipe_url:
+      "https://www.twopeasandtheirpod.com/peach-basil-mozzarella-balsamic-pizza/",
+    id: "664c8f193e7aa067e94e882f",
+  },
+  {
+    publisher: "Two Peas and Their Pod",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/avocadopizzawithcilantrosauce4bf5.jpg",
+    title: "Avocado Pita Pizza with Cilantro Sauce",
+    recipe_url:
+      "https://www.twopeasandtheirpod.com/avocado-pita-pizza-with-cilantro-sauce/",
+    id: "664c8f193e7aa067e94e880c",
+  },
+  {
+    publisher: "Bon Appetit",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/figandgoatcheesepizzawitharugula646698d.jpg",
+    title: "Fig and Goat Cheese Pizza with Arugula",
+    recipe_url:
+      "https://www.bonappetit.com/recipe/fig-and-goat-cheese-pizza-with-arugula",
+    id: "664c8f193e7aa067e94e877e",
+  },
+  {
+    publisher: "Closet Cooking",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/Avocado2Band2BFried2BEgg2BBreakfast2BPizza2B5002B296294dcea8a.jpg",
+    title: "Avocado Breakfast Pizza with Fried Egg",
+    recipe_url:
+      "https://www.closetcooking.com/avocado-breakfast-pizza-with-fried-egg/",
+    id: "664c8f193e7aa067e94e8476",
+  },
+  {
+    publisher: "Food Lover Recipes",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/20meals14_30007e78232.jpg",
+    title: "Artichoke Pizza With Lemony Green Bean Salad",
+    recipe_url:
+      "https://www.foodloversrecipes.com/2014/05/artichoke-pizzas-with-lemony-green-bean-salad/",
+    id: "664c8f193e7aa067e94e85a2",
+  },
+  {
+    publisher: "Closet Cooking",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/Thai2BChicken2BPizza2Bwith2BSweet2BChili2BSauce2B5002B435581bcf578.jpg",
+    title: "Thai Chicken Pizza with Sweet Chili Sauce",
+    recipe_url:
+      "https://www.closetcooking.com/thai-chicken-pizza-with-sweet-chili/",
+    id: "664c8f193e7aa067e94e84e2",
+  },
+  {
+    publisher: "Two Peas and Their Pod",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/sweetpotatokalepizza2c6db.jpg",
+    title: "Sweet Potato Kale Pizza with Rosemary & Red Onion",
+    recipe_url:
+      "https://www.twopeasandtheirpod.com/sweet-potato-kale-pizza-with-rosemary-red-onion/",
+    id: "664c8f193e7aa067e94e8823",
+  },
+  {
+    publisher: "Closet Cooking",
+    image_url:
+      "http://forkify-api.herokuapp.com/images/Strawberry2BBalsamic2BPizza2Bwith2BChicken252C2BSweet2BOnion2Band2BSmoked2BBacon2B5002B300939d125e2.jpg",
+    title:
+      "Balsamic Strawberry and Chicken Pizza with Sweet Onions and Smoked Bacon",
+    recipe_url:
+      "https://www.closetcooking.com/balsamic-strawberry-and-chicken-pizza/",
+    id: "664c8f193e7aa067e94e8482",
+  },
+];
