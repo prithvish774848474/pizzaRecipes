14358121396c1b9d5510a160ed06c0837448e7db
prithvish774848474 <prithvish.freelance@gmail.com>
2024-06-25 00:09:50 +0530

    Updated comments in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+/*
+Project Starting Date: 24th June, 2024
+Project First Deployment Date: 24th June, 2024
+*/
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
